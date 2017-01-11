95a96cc48642d6386a992dbfc26d07f2fd190e64
semistone222@gmail.com <semistone222@gmail.com>
2017-01-11 16:07:37 +0900

    T10.01 Exercise 완료
    - Intent를 이용해서 WaterReminderIntentService 호출

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -1,7 +1,28 @@
 package com.example.android.background.sync;
 
-// TODO (1) Create a class called ReminderTasks
+import android.content.Context;
 
+import com.example.android.background.utilities.PreferenceUtilities;
+
+// TODO (1) Create a class called ReminderTasks
+public class ReminderTasks {
+
+    public static final String ACTION_INCREMENT_WATER_COUNT = "action_increment_water_count";
+
+    public static void executeTask(Context context, String action) {
+        switch (action) {
+            case ACTION_INCREMENT_WATER_COUNT:
+                incrementWaterCount(context);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private static void incrementWaterCount(Context context) {
+        PreferenceUtilities.incrementWaterCount(context);
+    }
+}
 // TODO (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
 
 // TODO (6) Create a public static void method called executeTask
